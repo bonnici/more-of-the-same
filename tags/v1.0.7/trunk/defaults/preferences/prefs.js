83a93cee92dd74507b1610fed27d1f43dc271082
nickmarsh <nickmarsh@aa337dc2-1f74-11de-a78b-a7aca27b2395>
2009-11-09 06:15:51 +0000

    tagging v1.0.7

---
+++ tags/v1.0.7/trunk/defaults/preferences/prefs.js
@@ -0,0 +1,7 @@
+
+
+// Used to detect the first time the extension is run 
+pref("extensions.more-of-the-same.firstrun-v105", true);
+
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+// pref("extensions.{dc29ccec-fa18-144b-9b77-98b7debf3904}.description", "chrome://more-of-the-same/locale/more-of-the-same.properties");
